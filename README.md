7fefa5a05153c62cacfeb7c701460676bed99a09
joshuapjacob <xozhioc@gmail.com>
2021-06-14 00:50:59 +0200

    Really Almost Done

--- README.md
+++ README.md
@@ -1,25 +1,70 @@
-# fast-fourier-transform
-CSE305 Final Project
+# fast-fourier-transform-image-compression
 
-N.B. The program does not actually output a compressed jpeg image.
+An implementation and comparision of sequential and parallel fast Fourier transform algorithms for image compression.
+## Introduction  
+A fast Fourier transform algorithm quickly computes the discrete Fourier transform (DFT) of a sequence, or its inverse (IDFT). Fourier analysis converts a signal from its original domain (often time or space) to a representation in the frequency domain and vice versa. The DFT is obtained by decomposing a sequence of values into components of different frequencies.
+
+This project focuses on the application of fast Fourier transforms for image compression. A Fourier transform allows deconstructs an image to its frequncy domain. In this domain, one can remove insignificant frequncies, essentially stripping data from the image. One can then reconstruct the image with less frequncies to obtain a "compressed" image. For the sake of simplicity, image width and height are constrained to be a power of two and all images are greyscaled before fourier transform computations.
+
+In this project, four types algorithms were implemented and their execution times were analyzed based on the number of threads:
+1. Serial 2D FFT w/ Serial 1D FFT
+2. Parallel 2D FFT w/ Serial 1D FFT
+3. Serial 2D FFT w/ Parallel 1D FFT
+4. Parallel 2D FFT w/ Parallel 1D FFT
+
+Note: **The program does not actually output a compressed jpeg image.** It only shows what a compressed image would look like since the ouput is always a png.
+## Examples
+
+### images/grig.jpg (512x1024)
+Input | 10% | 2% | 1%
+:----:|:---:|:--:|:--:
+![](https://raw.githubusercontent.com/joshuapjacob/fast-fourier-transform-image-compression/main/images/grig.png) | ![](https://raw.githubusercontent.com/joshuapjacob/fast-fourier-transform-image-compression/main/images/compressed/grig_0.1.png) | ![](https://raw.githubusercontent.com/joshuapjacob/fast-fourier-transform-image-compression/main/images/compressed/grig_0.02.png) | [](https://raw.githubusercontent.com/joshuapjacob/fast-fourier-transform-image-compression/main/images/compressed/grig_0.01.png)
+
+Notice the loss of detail, particularly in the girl's eyebrows, and the man's earring disapearing at higher rates of compression.
+
+### images/graphics.jpg (512x512)
+Input | 10% | 5% | 1%
+:----:|:---:|:--:|:--:
+![](https://raw.githubusercontent.com/joshuapjacob/fast-fourier-transform-image-compression/main/images/graphics.png) | ![](https://raw.githubusercontent.com/joshuapjacob/fast-fourier-transform-image-compression/main/images/compressed/graphics_0.1.png) | ![](https://raw.githubusercontent.com/joshuapjacob/fast-fourier-transform-image-compression/main/images/compressed/graphics_0.05.png) | [](https://raw.githubusercontent.com/joshuapjacob/fast-fourier-transform-image-compression/main/images/compressed/graphics_0.01.png)
+
+Compression at lower rates removes the noise from this image (which is a render from [my ray tracer](https://github.com/joshuapjacob/computer-graphics))
+
+### `images/philip.jpg` (2048x2048)
+Input | 1% | 0.2% | 0.1%
+:----:|:--:|:----:|:----:
+![](https://raw.githubusercontent.com/joshuapjacob/fast-fourier-transform-image-compression/main/images/philip.png) | ![](https://raw.githubusercontent.com/joshuapjacob/fast-fourier-transform-image-compression/main/images/compressed/philip_0.01.png) | ![](https://raw.githubusercontent.com/joshuapjacob/fast-fourier-transform-image-compression/main/images/compressed/philip_0.002.png) | [](https://raw.githubusercontent.com/joshuapjacob/fast-fourier-transform-image-compression/main/images/compressed/philip_0.001.png)
+
+Notice the loss of detail, particularly in the hair.
+## Implementation Details
+
+TODO: Perform  a  detailed  comparison  of  the  resulting  algorithms.
+
+### 1. Serial 2D FFT w/ Serial 1D FFT
+### 2. Parallel 2D FFT w/ Serial 1D FFT
+### 3. Serial 2D FFT w/ Parallel 1D FFT
+### 4. Parallel 2D FFT w/ Parallel 1D FFT
+## Requirements
+- [stb](https://github.com/nothings/stb) (to read/write images)
+- [tbb](https://github.com/oneapi-src/oneTBB) (high level abstract threading for `std::sort`)
 
 ## Compilation & Execution
+Usage: ./main.out <image filename> <fraction of data to keep>
 ```
-$ g++ -O3 main.cpp -Wall -Wextra -pedantic -lpthread -D_GLIBCXX_PARALLEL -fopenmp -lttb -std=c++17 -o main.out
-$ ./main.out images/philip.png 0.05 # Keeps only 5% of the data in philip.png
+$ g++ -O3 main.cpp -D_GLIBCXX_PARALLEL -fopenmp -ltbb -std=c++17 -o main.out
+$ ./main.out images/philip.png 0.05 # keeps only 5% of the data in philip.png
 ```
-Output image is stored as `output.png`.
+The output images are stored as `output1.png`, `output2.png`, `output3.png`, and `output4.png`. All outputs are essentially the same apart from minuscule differences caused by numerial precision differences of the fast Fourier transform algorithms.
 
-Image width and height must be a power of two.
+## Analysis
 
-"usage: ./a.out <size of array> <number of threads>"
-## TODO
-- Do not slice in 2d transform
-- Perform  a  detailed  comparison  of  the  resulting  algorithms.
+Analysis of excution time was only performed for the largest image (`images/philip.png` 2048x2048).
+
 - Plot time vs. threads
 - Plot speed-up vs. threads
-- show different compression rates for different images
+## Conclusion
 
-## Bonus: Code Quality
-- No compiler warnings (with `-Wall -Wextra -pedantic`)
-- No memory leaks
+- It works? but not properlly parallelized.
+
+## References
+- [Image Compression and the FFT](https://www.youtube.com/watch?v=gGEBUdM0PVc)
+- [Image Compression and the FFT (Examples in Python)](https://www.youtube.com/watch?v=uB3v6n8t2dQ)
--- main.cpp
+++ main.cpp
@@ -18,7 +18,8 @@ const double PI = 3.141592653589793238460;
 
 typedef std::complex<double> Complex;
 
-// 1D COOLEY-TUKEY RADIX-2 DECIMATION-IN-FREQUENCY FAST FOURIER TRANSFORM ------
+// SERIAL 1D FAST FOURIER TRANSFORM --------------------------------------------
+// COOLEY-TUKEY RADIX-2 DECIMATION-IN-FREQUENCY
 
 void fft_radix2(std::valarray<Complex> &x) {
     const size_t N = x.size();
@@ -41,38 +42,86 @@ void inverse_fft_radix2(std::valarray<Complex> &x) {
     x /= x.size();
 }
 
-// OPTIMIZED PARALLEL 1D FAST FOURIER TRANSFORM --------------------------------
+// PARALLEL 1D FAST FOURIER TRANSFORM ------------------------------------------
+// COOLEY-TUKEY RADIX-2 DECIMATION-IN-TIME
 
-void parallel_fft_radix2(std::valarray<Complex> &x) {
-    // TODO
-    const size_t N = x.size();
-    if (N <= 1) return;
-    std::valarray<Complex> even = x[std::slice(0, N/2, 2)];
-    std::valarray<Complex> odd = x[std::slice(1, N/2, 2)];
-    fft_radix2(even);
-    fft_radix2(odd);
-    for (size_t k = 0; k < N/2; k++) {
-        Complex t = std::polar(1.0, -2 * PI * k / N) * odd[k];
-        x[k] = even[k] + t;
-        x[k+N/2] = even[k] - t;
+size_t reverse_bits(size_t x, int n) {
+	size_t res = 0;
+	for (int i = 0; i < n; i++, x >>= 1) {
+        res = (res << 1) | (x & 1U);
     }
+	return res;
 }
 
-void parallel_inverse_fft_radix2(std::valarray<Complex> &x) {
+void compute_exp_table_block(
+        std::valarray<Complex> &exp_table, size_t N, uint start, uint stop
+    ) {
+    for (size_t k = start; k < stop; k++) {
+        exp_table[k] = std::polar(1.0, -2 * PI * k / N);
+    }	
+}
+
+void parallel_fft_radix2(std::valarray<Complex> &x, uint num_threads) {
+    // NOTE: Not Properly Parallelized
+    // TODO: Share exp_table with all 1D FFTs when 2D FFT.
+    const size_t N = x.size();
+    uint levels = 0;
+	for (size_t temp = N; temp > 1U; temp >>= 1) levels++;
+    const size_t exp_table_size = N/2;
+    std::valarray<Complex> exp_table(exp_table_size);
+    std::vector<std::thread> threads(num_threads - 1);
+    uint block_size = (exp_table_size)/num_threads;
+    uint block_start = 0;
+    uint block_end = block_size;
+    for (uint i = 0; i < (num_threads - 1); i++) {
+        threads[i] = std::thread(
+            compute_exp_table_block,
+            std::ref(exp_table), N,
+            block_start, block_end
+        );
+        block_start += block_size;
+        block_end += block_size;
+    }
+    compute_exp_table_block(exp_table, N, block_start, exp_table_size);
+    std::for_each(threads.begin(), threads.end(),
+        std::mem_fn(&std::thread::join)
+    );
+    for (uint i = 0; i < N; i++) {
+		size_t j = reverse_bits(i, levels);
+		if (j > i)
+			std::swap(x[i], x[j]);
+	}
+	for (uint size = 2; size <= N; size *= 2) {
+        size_t halfsize = size / 2;
+		size_t tablestep = N / size;
+        for (uint i = 0; i < N; i += size) {
+			for (uint j = i, k = 0; j < i + halfsize; j++, k += tablestep) {
+				Complex temp = x[j + halfsize] * exp_table[k];
+				x[j + halfsize] = x[j] - temp;
+				x[j] += temp;
+            }
+        }
+		if (size == N) break;
+	}
+}
+
+void parallel_inverse_fft_radix2(std::valarray<Complex> &x, uint num_threads) {
     x = x.apply(std::conj);
-    parallel_fft_radix2(x);
+    parallel_fft_radix2(x, num_threads);
     x = x.apply(std::conj);
     x /= x.size();
 }
 
-// 2D FAST FOURIER TRANSFORM UTILITY FUNCTIONS ---------------------------------
+// SERIAL 2D FAST FOURIER TRANSFORM --------------------------------------------
 
 void fft_rows(
-        std::valarray<Complex> &x, int width, int height, int start, int end
+        std::valarray<Complex> &x, int width, int height, int start, int end,
+        uint num_sub_threads = 1
     ) {
     for (int i = start; i < end; i++) {
         std::valarray<Complex> sliced = x[std::slice(i*width,width,1)];
-        fft_radix2(sliced);
+        if (num_sub_threads == 1) fft_radix2(sliced);
+        else parallel_fft_radix2(sliced, num_sub_threads);
         for (size_t k = 0; k < sliced.size(); k++) {
             x[i*width+k] = sliced[k];
         }
@@ -80,11 +129,13 @@ void fft_rows(
 }
 
 void fft_columns(
-        std::valarray<Complex> &x, int width, int height, int start, int end
+        std::valarray<Complex> &x, int width, int height, int start, int end,
+        uint num_sub_threads = 1
     ) {
     for (int j = start; j < end; j++) {
         std::valarray<Complex> sliced = x[std::slice(j,height,width)];
-        fft_radix2(sliced);
+        if (num_sub_threads == 1) fft_radix2(sliced);
+        else parallel_fft_radix2(sliced, num_sub_threads);
         for (size_t k = 0; k < sliced.size(); k++)
         {
             x[j+k*width] = sliced[k];
@@ -93,11 +144,13 @@ void fft_columns(
 }
 
 void inverse_fft_rows(
-        std::valarray<Complex> &x, int width, int height, int start, int end
+        std::valarray<Complex> &x, int width, int height, int start, int end,
+        uint num_sub_threads = 1
     ) {
     for (int i = start; i < end; i++) {
         std::valarray<Complex> sliced = x[std::slice(i*width,width,1)];
-        inverse_fft_radix2(sliced);
+        if (num_sub_threads == 1) inverse_fft_radix2(sliced);
+        else parallel_inverse_fft_radix2(sliced, num_sub_threads);
         for (size_t k = 0; k < sliced.size(); k++) {
             x[i*width+k] = sliced[k];
         }
@@ -105,11 +158,13 @@ void inverse_fft_rows(
 }
 
 void inverse_fft_columns(
-        std::valarray<Complex> &x, int width, int height, int start, int end
+        std::valarray<Complex> &x, int width, int height, int start, int end,
+        uint num_sub_threads = 1
     ) {
     for (int j = start; j < end; j++) {
         std::valarray<Complex> sliced = x[std::slice(j,height,width)];
-        inverse_fft_radix2(sliced);
+        if (num_sub_threads == 1) inverse_fft_radix2(sliced);
+        else parallel_inverse_fft_radix2(sliced, num_sub_threads);
         for (size_t k = 0; k < sliced.size(); k++)
         {
             x[j+k*width] = sliced[k];
@@ -117,23 +172,33 @@ void inverse_fft_columns(
     }
 }
 
-// SERIAL 2D FAST FOURIER TRANSFORM --------------------------------------------
-
-void fft_image(std::valarray<Complex> &x, int width, int height) {
-    fft_rows(x, width, height, 0, height);
-    fft_columns(x, width, height, 0, width);
-}
-
-void inverse_fft_image(std::valarray<Complex> &x, int width, int height) {
-    inverse_fft_columns(x, width, height, 0, width);
-    inverse_fft_rows(x, width, height, 0, height);
-}
-
-// SIMPLE PARALLEL 2D FAST FOURIER TRANSFORM -----------------------------------
-
-void simple_parallel_fft_image(
-        std::valarray<Complex> &x, int width, int height, uint num_threads
+void fft_image(
+        std::valarray<Complex> &x, int width, int height,
+        uint num_sub_threads = 1
     ) {
+    fft_rows(x, width, height, 0, height, num_sub_threads);
+    fft_columns(x, width, height, 0, width, num_sub_threads);
+}
+
+void inverse_fft_image(
+        std::valarray<Complex> &x, int width, int height,
+        uint num_sub_threads = 1
+    ) {
+    inverse_fft_columns(x, width, height, 0, width, num_sub_threads);
+    inverse_fft_rows(x, width, height, 0, height, num_sub_threads);
+}
+
+// PARALLEL 2D FAST FOURIER TRANSFORM -----------------------------------
+
+void parallel_fft_image(
+        std::valarray<Complex> &x, int width, int height, uint num_threads,
+        bool double_parallel = false
+    ) {
+    uint num_sub_threads = 1;
+    if (double_parallel) {
+        num_threads /= 2;
+        num_sub_threads = 2;
+    }
     uint block_size = height/num_threads;
     std::vector<std::thread> threads(num_threads - 1);
     uint block_start = 0;
@@ -142,7 +207,7 @@ void simple_parallel_fft_image(
         threads[i] = std::thread(
             fft_rows,
             std::ref(x), width, height,
-            block_start, block_end
+            block_start, block_end, num_sub_threads
         );
         block_start += block_size;
         block_end += block_size;
@@ -158,7 +223,7 @@ void simple_parallel_fft_image(
         threads[i] = std::thread(
             fft_columns,
             std::ref(x), width, height,
-            block_start, block_end
+            block_start, block_end, num_sub_threads
         );
         block_start += block_size;
         block_end += block_size;
@@ -169,9 +234,15 @@ void simple_parallel_fft_image(
     );
 }
 
-void simple_parallel_inverse_fft_image(
-        std::valarray<Complex> &x, int width, int height, uint num_threads
+void parallel_inverse_fft_image(
+        std::valarray<Complex> &x, int width, int height, uint num_threads,
+        bool double_parallel = false
     ) {
+    uint num_sub_threads = 1;
+    if (double_parallel) {
+        num_threads /= 2;
+        num_sub_threads = 2;
+    }
     uint block_size = width/num_threads;
     std::vector<std::thread> threads(num_threads - 1);
     uint block_start = 0;
@@ -180,7 +251,7 @@ void simple_parallel_inverse_fft_image(
         threads[i] = std::thread(
             inverse_fft_columns,
             std::ref(x), width, height,
-            block_start, block_end
+            block_start, block_end, num_sub_threads
         );
         block_start += block_size;
         block_end += block_size;
@@ -196,7 +267,7 @@ void simple_parallel_inverse_fft_image(
         threads[i] = std::thread(
             inverse_fft_rows,
             std::ref(x), width, height,
-            block_start, block_end
+            block_start, block_end, num_sub_threads
         );
         block_start += block_size;
         block_end += block_size;
@@ -207,10 +278,6 @@ void simple_parallel_inverse_fft_image(
     );
 
 }
-
-// OPTIMIZED PARALLEL 2D FAST FOURIER TRANSFORM --------------------------------
-
-// TODO
 
 // COMPRESSION -----------------------------------------------------------------
 
@@ -253,15 +320,15 @@ double find_cut_off(std::valarray<Complex> &x, double keep) {
         }
     } comp;
     std::sort(
-        std::execution::par_unseq,
+        std::execution::par,
         std::begin(*sorted_x), std::end(*sorted_x),
         comp
     );
     size_t cut_off_index = std::round(keep*x.size());
-    return abs((*sorted_x)[cut_off_index]);
+    double cut_off_val = abs((*sorted_x)[cut_off_index]);
+    delete sorted_x;
+    return cut_off_val;
 }
-
-
 
 // UTILITY FUNCTIONS -----------------------------------------------------------
 
@@ -290,8 +357,7 @@ std::valarray<Complex>* read_img(
     Complex *img_array =  new Complex[grey_img_size];
 
     size_t k = 0;
-    // TODO: Parallelize.
-    for (uint8_t* p = rgb_img; p != rgb_img + rgb_img_size; p += channels) {
+    for (uint8_t *p = rgb_img; p != rgb_img + rgb_img_size; p += channels) {
         img_array[k] = 0.2989*(*p) + 0.5870*(*(p + 1)) + 0.1140*(*(p + 2));
         k++;
     }
@@ -312,7 +378,6 @@ void write_img(
     ) {
     size_t out_img_size = width*height;
     uint8_t out_img[out_img_size];
-    // TODO: Parallelize.
     for (size_t k = 0; k < out_img_size; k++) {
         out_img[k] = (uint8_t) std::max(std::min(255., img_data[k].real()),0.);
     }
@@ -347,10 +412,13 @@ int main(int argc, char const *argv[]) {
         exit(1);
     }
 
-    uint const n = 4; // std::thread::hardware_concurrency();
+    uint const n = std::thread::hardware_concurrency();
     std::cout << "Number of threads: " << n << std::endl << std::endl;
 
     // TEST: Serial 2D FFT w/ Serial 1D FFT
+    std::cout <<
+        "TEST: Serial 2D FFT w/ Serial 1D FFT"
+    << std::endl;
 
     int width, height;
     std::valarray<Complex> *img_data = read_img(
@@ -358,7 +426,7 @@ int main(int argc, char const *argv[]) {
     );
 
     std::cout << "FFT Image Duration: " <<
-        time_it(fft_image, *img_data, width, height) << "s" << std::endl;
+        time_it(fft_image, *img_data, width, height, 1) << "s" << std::endl;
 
     double cut_off = find_cut_off(*img_data, keep); // WARNING: SLOW
 
@@ -366,42 +434,81 @@ int main(int argc, char const *argv[]) {
         time_it(quick_compress, *img_data, cut_off, n) << "s" << std::endl;
 
     std::cout << "Inverse FFT Image Duration: " <<
-        time_it(inverse_fft_image, *img_data, width, height) << "s" << std::endl;
+        time_it(inverse_fft_image, *img_data, width, height, 1) << "s" << std::endl;
 
     write_img(*img_data, width, height, "output1.png");
     delete img_data;
 
     std::cout << std::endl;
 
-    // TEST: Simple Parallel 2D FFT w/ Serial 1D FFT
+    // TEST: Parallel 2D FFT w/ Serial 1D FFT
+    std::cout <<
+        "TEST: Parallel 2D FFT w/ Serial 1D FFT"
+    << std::endl;
 
     img_data = read_img(
         input_image_filename, width, height
     );
 
-    std::cout << "Naive Parallel FFT Image Duration: " <<
-        time_it(simple_parallel_fft_image, *img_data, width, height, n) << "s" << std::endl;
+    std::cout << "Parallel FFT Image Duration: " <<
+        time_it(parallel_fft_image, *img_data, width, height, n, false) << "s" << std::endl;
 
     std::cout << "Quick Compression Duration: " <<
         time_it(quick_compress, *img_data, cut_off, n) << "s" << std::endl;
 
-    std::cout << "Naive Parallel Inverse FFT Image Duration: " <<
-        time_it(simple_parallel_inverse_fft_image, *img_data, width, height, n) << "s" << std::endl;
+    std::cout << "Parallel Inverse FFT Image Duration: " <<
+        time_it(parallel_inverse_fft_image, *img_data, width, height, n, false) << "s" << std::endl;
 
     write_img(*img_data, width, height, "output2.png");
+    delete img_data;
 
-    // TEST: Optimized Parallel 2D FFT w/ Serial 1D FFT
-    // TODO
+    std::cout << std::endl;
+
+    img_data = read_img(
+        input_image_filename, width, height
+    );
 
     // TEST: Serial 2D FFT w/ Parallel 1D FFT
-    // TODO
+    std::cout <<
+        "TEST: Serial 2D FFT w/ Parallel 1D FFT"
+    << std::endl;
 
-    // TEST: Simple Parallel 2D FFT w/ Parallel 1D FFT
-    // TODO
+    std::cout << "FFT Image Duration: " <<
+        time_it(fft_image, *img_data, width, height, n) << "s" << std::endl;
 
-    // TEST: Optimized Parallel 2D FFT w/ Parallel 1D FFT
-    // TODO
+    std::cout << "Quick Compression Duration: " <<
+        time_it(quick_compress, *img_data, cut_off, n) << "s" << std::endl;
 
+    std::cout << "Inverse FFT Image Duration: " <<
+        time_it(inverse_fft_image, *img_data, width, height, n) << "s" << std::endl;
+
+    write_img(*img_data, width, height, "output3.png");
     delete img_data;
+    
+    std::cout << std::endl;
+
+    // TEST: Parallel 2D FFT w/ Parallel 1D FFT
+    std::cout <<
+        "TEST: Parallel 2D FFT w/ Parallel 1D FFT"
+    << std::endl;
+    
+    img_data = read_img(
+        input_image_filename, width, height
+    );
+
+    std::cout << "Parallel FFT Image Duration: " <<
+        time_it(parallel_fft_image, *img_data, width, height, n, true) << "s" << std::endl;
+
+    std::cout << "Quick Compression Duration: " <<
+        time_it(quick_compress, *img_data, cut_off, n) << "s" << std::endl;
+
+    std::cout << "Parallel Inverse FFT Image Duration: " <<
+        time_it(parallel_inverse_fft_image, *img_data, width, height, n, true) << "s" << std::endl;
+
+    write_img(*img_data, width, height, "output4.png");
+    delete img_data;
+
+    std::cout << std::endl;
+
     return 0;
 }
